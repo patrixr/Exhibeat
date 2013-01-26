6070a0af3b86c98afdc7a82d95c782153e2366dd
Patrick Rabier <patrick.rabier@gmail.com>
2013-01-26 15:42:51 +0800

    Premiere gestion des evenements temporels

---
+++ project/Exhibeat/Exhibeat/Rhythm/ITimeEventEmitter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exhibeat.Rhythm
+{
+    public interface ITimeEventEmitter
+    {
+        /// <summary>
+        /// The emitter will save the reciever in order to notify future events to it
+        /// </summary>
+        /// <param name="rcv"></param>
+        /// <returns></returns>
+        public bool RegisterNewReciever(ITimeEventReciever rcv, bool songEvents = true, bool userEvents = true);
+        public bool UnregisterReciever(ITimeEventReciever rcv);
+        public void ClearAllReceivers();
+    }
+}
---
+++ project/Exhibeat/Exhibeat/Rhythm/ITimeEventReciever.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exhibeat.Rhythm
+{
+    public interface ITimeEventReciever
+    {
+        /// <summary>
+        /// The reciever is notified of a change in the music
+        /// </summary>
+        /// <param name="ev"></param>
+        public void NewSongEvent(songEvent ev, int param);
+
+        /// <summary>
+        /// The reciever is notified of a new user event, a key press
+        /// </summary>
+        /// <param name="ev"></param>
+        /// <param name="param"></param>
+        public void NewUserEvent(userEvent ev, int param);
+    }
+}
