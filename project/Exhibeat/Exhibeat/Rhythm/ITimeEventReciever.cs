a1365b236458a00ba260208f4de7b0a69306c23c
Paco Martinez <nashmun@epitech.eu>
2013-02-12 16:16:27 +0800

    fixed merge issue

--- project/Exhibeat/Exhibeat/Rhythm/ITimeEventReciever.cs
+++ project/Exhibeat/Exhibeat/Rhythm/ITimeEventReciever.cs
@@ -11,21 +11,13 @@ namespace Exhibeat.Rhythm
         /// The reciever is notified of a change in the music
         /// </summary>
         /// <param name="ev"></param>
-<<<<<<< HEAD
         public void NewSongEvent(songEvent ev, Object param);
-=======
-        void NewSongEvent(songEvent ev, int param);
->>>>>>> a7a1f5b9cc71b87c4fafb1fae1e9fb18046b1d49
 
         /// <summary>
         /// The reciever is notified of a new user event, a key press
         /// </summary>
         /// <param name="ev"></param>
         /// <param name="param"></param>
-<<<<<<< HEAD
         public void NewUserEvent(userEvent ev, Object param);
-=======
-        void NewUserEvent(userEvent ev, int param);
->>>>>>> a7a1f5b9cc71b87c4fafb1fae1e9fb18046b1d49
     }
 }
