a32379d4827037e4fe3f7bb424bdbdd5406f1d18
Patrick <patrick.rabier@gmail.com>
2013-08-11 21:34:27 +0200

    New lifebar, and beginning of scorescreen

---
+++ project/Exhibeat/Exhibeat/Screens/ScoreScreen.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Humble;
+using Humble.Screens;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Exhibeat.Screens
+{
+    class ScoreScreen : Screen
+    {
+        public ScoreScreen(HumbleGame game)
+            : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void HandleInput()
+        {
+            base.HandleInput();
+        }
+
+        public override void Draw()
+        {
+            base.Draw();
+        }
+    }
+}
