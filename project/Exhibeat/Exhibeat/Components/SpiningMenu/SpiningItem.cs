fc1125629e156d0545d23df189b17aaf8eadc7b3
Sadler <julien.darsel@epitech.eu>
2013-08-12 11:05:11 -0700

    scan des repertoire mais le songSelectionScreen n'est pas fini

---
+++ project/Exhibeat/Exhibeat/Components/SpiningMenu/SpiningItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Exhibeat.Components.SpiningMenu
+{
+    class SpiningItem
+    {
+        public String title = "TITLE";
+        public String subTitle = "SUBTITLE";
+        public String text = "TEXT";
+        public Texture2D thumbnail = null;
+    }
+}
---
+++ project/Exhibeat/Exhibeat/Tools/ExhibeatDirectoryScan.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Exhibeat_SongDirectory_Scan
+{
+    public class ExhibeatDirectoryScan
+    {
+        private static string fileName = "ExhibeatSongFile.xml";
+        private List<string> songPath = new List<string>();
+
+        public void ScanSongDirectory(string targetDirectory)
+        {
+            ProcessDirectory(targetDirectory);
+            if (songPath.Count > 0)
+                savePathXML(targetDirectory);
+        }
+
+        public void ProcessDirectory(string targetDirectory) 
+        {
+            if (Directory.Exists(targetDirectory)) 
+            {
+                string [] fileEntries = Directory.GetFiles(targetDirectory);
+                foreach(string fileName in fileEntries)
+                    ProcessFile(fileName);
+
+                string [] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+                foreach(string subdirectory in subdirectoryEntries)
+                    ProcessDirectory(subdirectory);
+            }
+            else 
+            {
+                Console.WriteLine("{0} is not a valid file or directory.", targetDirectory);
+            }  
+
+        }
+        public void ProcessFile(string path)
+        {
+            if (Path.GetExtension(path) == ".exi")
+            {
+                songPath.Add(path);
+            }
+        }
+       
+        public List<string> loadPathXML(string xmlPath)
+        {
+            List<string> songPath = new List<string>();
+            if (File.Exists(xmlPath + "/" + fileName))
+            {
+                XmlReader reader = XmlReader.Create(xmlPath + "/" + fileName);
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element && reader.Name == "path")
+                    {
+                        reader.Read();
+                        songPath.Add(reader.Value);
+                    }
+                }
+            }
+            return songPath;
+        }
+        public void savePathXML(string xmlPath)
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlElement el = (XmlElement)doc.AppendChild(doc.CreateElement("xml"));
+            foreach (string path in songPath)
+                el.AppendChild(doc.CreateElement("path")).InnerText = path;
+            if (File.Exists(xmlPath + "/" + fileName))
+            {
+                File.WriteAllText(xmlPath + "/" + fileName, string.Empty);
+                System.IO.File.WriteAllText(xmlPath + "/" + fileName, doc.OuterXml);
+            }
+            else
+                System.IO.File.WriteAllText(xmlPath + "/" + fileName, doc.OuterXml);
+        }
+
+    }
+}
