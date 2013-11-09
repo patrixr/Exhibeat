3aa7460b715dfb378bb82b01a893f42eba56b403
Julien <julien.darsel@gmail.com>
2013-11-09 16:01:12 +0100

    integration du gamepad a decommenter

--- project/Exhibeat/Exhibeat/KeyReader/KeyReader.cs
+++ project/Exhibeat/Exhibeat/KeyReader/KeyReader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
 namespace ExhiBeat.KeyReader
@@ -10,6 +11,7 @@ namespace ExhiBeat.KeyReader
     {
         private bool[]      _curState;
         private List<Key>  _keyEvents;
+        private GamePadState previousGamePadState;
 
         public KeyReader()
         {
@@ -19,6 +21,8 @@ namespace ExhiBeat.KeyReader
             _keyEvents = new List<Key>();
             for (i = 0; i < _curState.Length; i++)
                 _curState[i] = false;
+
+            previousGamePadState = GamePad.GetState(PlayerIndex.One);
         }
 
         private void createNewKeyEvent(int pos, keyType type)
@@ -28,6 +32,24 @@ namespace ExhiBeat.KeyReader
             key.pos = pos;
             key.type = type;
             _keyEvents.Add(key);
+        }
+
+        private void checkGamePadKey(GamePadState state, Buttons butt, int pos)
+        {
+            // Console.WriteLine("Check");
+            if (state.IsButtonDown(butt))
+            {
+                if (_curState[pos] != true)
+                {
+                    _curState[pos] = true;
+                    createNewKeyEvent(pos, keyType.pressed);
+                }
+            }
+            else if (_curState[pos] == true)
+            {
+                _curState[pos] = false;
+                createNewKeyEvent(pos, keyType.realeased);
+            }
         }
 
         private void checkKey(KeyboardState state, Keys key, int pos)
@@ -49,6 +71,7 @@ namespace ExhiBeat.KeyReader
             }
         }
 
+        
         private void refresh()
         {
             KeyboardState state = Keyboard.GetState();
@@ -60,6 +83,15 @@ namespace ExhiBeat.KeyReader
             checkKey(state, Keys.Z, 5);
             checkKey(state, Keys.A, 2);
             checkKey(state, Keys.W, 0);
+
+            /*GamePadState state2 = GamePad.GetState(PlayerIndex.One);
+            checkGamePadKey(state2, Buttons.B, 6);
+            checkGamePadKey(state2, Buttons.A, 5);
+            checkGamePadKey(state2, Buttons.RightShoulder, 4);
+            checkGamePadKey(state2, Buttons.Start, 3);
+            checkGamePadKey(state2, Buttons.LeftShoulder, 2);
+            checkGamePadKey(state2, Buttons.Y, 1);
+            checkGamePadKey(state2, Buttons.X, 0);*/
         }
 
         public Key getNextKeyEvent()
